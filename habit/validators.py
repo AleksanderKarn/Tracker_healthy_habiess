8cd8582f221f38468b612748ecfdfdc92673467a
ganst <qanstashet_911@mail.ru>
2023-03-21 15:39:17 +0300

    исправил баг с валидаторами - тесты проходят 86%

--- habit/validators.py
+++ habit/validators.py
@@ -5,16 +5,16 @@ from habit.models import Habit
 
 class HabitCreateValidatr:
     pleasant_habit_id = Habit.objects.all().filter(is_pleasant=True)
+
     def __init__(self, field):
         self.field = field
 
     def __call__(self, value):
-        is_pleasant =value.get('is_pleasant')
+        is_pleasant = value.get('is_pleasant')
         pleasant_habit = value.get('pleasant_habit')
         award = value.get('award')
         period = value.get('period')
         time_to_complete = value.get('time_to_complete')
-        is_public = value.get('is_public')
 
         if is_pleasant == True:  # если привычка приятная
             if award or pleasant_habit:
@@ -28,6 +28,8 @@ class HabitCreateValidatr:
             if award and pleasant_habit:
                 raise serializers.ValidationError("Может быть или вознаграждение или приятная привычка!")
             elif not award and not pleasant_habit:
-                raise serializers.ValidationError("нельзя, чтобы связанная привычка и вознаграждение были одновременно пустые")
-            elif pleasant_habit not in self.pleasant_habit_id:
-                raise serializers.ValidationError("в связанные привычки могут попадать только привычки с признаком приятной привычки")
+                raise serializers.ValidationError(
+                    "нельзя, чтобы связанная привычка и вознаграждение были одновременно пустые")
+            elif pleasant_habit and pleasant_habit not in self.pleasant_habit_id:
+                raise serializers.ValidationError(
+                    "в связанные привычки могут попадать только привычки с признаком приятной привычки")
